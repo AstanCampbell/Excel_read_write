d572289d6f8cecec0ffbc79fbcd519f6717985fe
Astan Campbell <astan.campbell@gmail.com>
2020-11-08 14:25:05 +0000

    Update Looping_analysis.py
    removed dead code

--- Looping_analysis.py
+++ Looping_analysis.py
@@ -2,6 +2,7 @@
 # Reading an excel file using Python
 import xlrd
 import xlwt
+import pandas as pd
 
 # Give the location of the file
 loc = ("C:Documents\\Hackathon\\textAnalysis.xls")
@@ -9,18 +10,9 @@ loc = ("C:Documents\\Hackathon\\textAnalysis.xls")
 wb = xlrd.open_workbook('Planning_Data_Final2.xls', on_demand = True)
 sheet = wb.sheet_by_name('Planning_Data_Final')
 
-row_num = 13
 
 # For row 0 and column 0 the cells and columns are indexed just like arrays.
-
 #analyses the cell which is indexed below
-str = sheet.cell(row_num,2).value
-#print("-- Number of potential units  :")
-units = [int(s) for s in str.split() if s.isdigit()]
-#print(units)
-
-total_units = sum(units)
-#print(total_units)
 
 for row_index in range(1, sheet.nrows):
     str = sheet.cell(row_index,2).value
